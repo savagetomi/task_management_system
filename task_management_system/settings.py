4c66c2e37a7c5a7a9bbcb49e929f9df6142d2e33
Epaphras Savage <dhareykhaey3@gmail.com>
2026-07-25 03:02:38 +0100

    Add Swagger API documentation

--- task_management_system/settings.py
+++ task_management_system/settings.py
@@ -63,9 +63,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
     ],
-    "DEFAULT_SCHEMA_CLASS": [
-        "drf_spectacular.openapi.AutoSchema"
-    ],
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_AUTHENTICATION_CLASSES": (
             "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
